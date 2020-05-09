f5dbec4bead3f08a908f244f11d3bcd0da2a66e0
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-05-10 01:29:46 +0800

    release: 1.0.0

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["stylelint-config-twbs-bootstrap/css"],
+  rules: {
+    "selector-max-class": 8,
+    "selector-max-combinators": 8,
+    "selector-max-compound-selectors": 8,
+    "selector-no-qualifying-type": [
+      true,
+      { ignore: ["attribute", "class", "id"] },
+    ],
+    "selector-type-no-unknown": [
+      true,
+      { ignore: ["custom-elements", "default-namespace"] },
+    ],
+    "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
+  },
+};
